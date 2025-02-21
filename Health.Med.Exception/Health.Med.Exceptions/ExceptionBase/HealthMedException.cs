a488a5444ab2ad951fdfa4d9688d37fb79bac399
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2025-02-21 10:23:26 -0300

    add exceptions project

---
+++ Health.Med.Exception/Health.Med.Exceptions/ExceptionBase/HealthMedException.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Health.Med.Exceptions.ExceptionBase;
+
+public class HealthMedException : SystemException
+{
+    public HealthMedException(string mensagem) : base(mensagem)
+    {
+    }
+
+    protected HealthMedException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
---
+++ Health.Med.Exception/Health.Med.Exceptions/ExceptionBase/ValidationErrorsException.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Health.Med.Exceptions.ExceptionBase;
+
+[Serializable]
+public class ValidationErrorsException : HealthMedException
+{
+    public List<string> ErrorMessages { get; set; } = [];
+    public ValidationErrorsException(List<string> errorMessages) : base(string.Empty)
+    {
+        ErrorMessages = errorMessages;
+    }
+
+    protected ValidationErrorsException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
