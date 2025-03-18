2a89a29eb08e4c8d6695addd1b62c40fdb7d65f1
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2025-03-17 23:08:16 -0300

    feat: added rabbit mq listener

---
+++ Health.Med.Notification/Notification.Api/Listeners/SendMailListener.cs
@@ -0,0 +1,52 @@
+﻿using Notification.Api.Mapping;
+using Notification.Api.QueueModels;
+using Notification.Application.UseCase.SendMail;
+using Notification.Infrastructure.Queue;
+using RabbitMQ.Client;
+
+namespace Notification.Api.Listeners;
+
+public class SendMailListener(
+    IConnectionFactory connectionFactory,
+    Serilog.ILogger logger,
+    IServiceScopeFactory scopeFactory)
+    : QueueListenerBase<SendMailModel>(
+        RabbitMqConstants.NotificationQueueName,
+        connectionFactory)
+{
+    private readonly Serilog.ILogger _logger = logger;
+    private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
+
+    protected override async Task ProcessMessageAsync(SendMailModel message)
+    {
+        try
+        {
+            _logger.Information($"{nameof(SendMailListener)} - Início do processamento da mensagem.");
+
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var sendMailUseCase = scope.ServiceProvider.GetRequiredService<ISendMailUseCase>();
+
+                var sendMail = await sendMailUseCase.SendAsync(message.MessageToRequest());
+
+                if (!sendMail.IsSuccess())
+                {
+                    _logger.Error($"{nameof(SendMailListener)} - Ocorreu um erro ao enviar o e-mail. {sendMail.Errors}");
+                }
+            }
+
+            _logger.Information($"{nameof(SendMailListener)} - Fim do processamento da mensagem.");
+        }
+        catch (Exception ex)
+        {
+            await ProcessErrorAsync(ex);
+        }
+    }
+
+    protected override async Task ProcessErrorAsync(Exception ex)
+    {
+        _logger.Error($"Ocorreu um erro durante o processamento da mensagem. {ex.Message}");
+
+        await Task.CompletedTask;
+    }
+}
--- Health.Med.Notification/Notification.Api/Program.cs
+++ Health.Med.Notification/Notification.Api/Program.cs
@@ -1,5 +1,6 @@
 using Notification.Infrastructure;
 using Notification.Application;
+using Notification.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddConfiguration(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 
