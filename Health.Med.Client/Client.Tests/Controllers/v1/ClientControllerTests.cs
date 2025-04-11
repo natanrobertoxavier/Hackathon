98ee8b695b885b304c3eb723d016879d0c3c8fa7
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2025-04-10 22:12:18 -0300

    client : added unit tests

---
+++ Health.Med.Client/Client.Tests/Controllers/v1/ClientControllerTests.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Client.Api.Controllers.v1;
+using Client.Application.UseCase.ChangePassword;
+using Client.Application.UseCase.Recover.RecoverAll;
+using Client.Application.UseCase.Recover.RecoverByCPF;
+using Client.Application.UseCase.Recover.RecoverByEmail;
+using Client.Application.UseCase.Register;
+using Client.Communication.Request;
+using Client.Communication.Response;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Client.Tests.Controllers.v1;
+
+public class ClientControllerTests
+{
+    [Fact]
+    public async Task RegisterUserAsync_ShouldReturnCreated_WhenUseCaseSucceeds()
+    {
+        // Arrange
+        var mockUseCase = new Mock<IRegisterUseCase>();
+        var request = new RequestRegisterClient(
+            "John Doe",
+            "John",
+            "john.doe@example.com",
+            "12345678900",
+            "password123");
+
+        var result = new Result<MessageResult>();
+        result.Succeeded(new MessageResult("Client registered successfully"));
+
+        mockUseCase.Setup(x => x.RegisterClientAsync(request)).ReturnsAsync(result);
+
+        var controller = new ClientController();
+
+        // Act
+        var response = await controller.RegisterUserAsync(mockUseCase.Object, request) as ObjectResult;
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal((int)HttpStatusCode.Created, response.StatusCode);
+        Assert.Equal(result, response.Value);
+    }
+
+    [Fact]
+    public async Task ChangePasswordAsync_ShouldReturnOk_WhenUseCaseSucceeds()
+    {
+        // Arrange
+        var mockUseCase = new Mock<IChangePasswordUseCase>();
+        var request = new RequestChangePassword(
+            "oldPassword123",
+            "newPassword123");
+
+        var result = new Result<MessageResult>();
+        result.Succeeded(new MessageResult("Password changed successfully"));
+
+        mockUseCase.Setup(x => x.ChangePasswordAsync(request)).ReturnsAsync(result);
+
+        var controller = new ClientController();
+
+        // Act
+        var response = await controller.ChangePasswordAsync(mockUseCase.Object, request) as ObjectResult;
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(result, response.Value);
+    }
+
+    [Fact]
+    public async Task RecoverAllAsync_ShouldReturnOk_WhenUseCaseSucceeds()
+    {
+        // Arrange
+        var mockUseCase = new Mock<IRecoverAllUseCase>();
+
+        var result = new Result<IEnumerable<ResponseClient>>();
+        result.Succeeded(new List<ResponseClient>
+        {
+            new ResponseClient(Guid.NewGuid(), DateTime.UtcNow, "John Doe", "john.doe@example.com", "12345678900")
+        });
+
+        mockUseCase.Setup(x => x.RecoverAllAsync(1, 5)).ReturnsAsync(result);
+
+        var controller = new ClientController();
+
+        // Act
+        var response = await controller.RecoverAllAsync(mockUseCase.Object, 1, 5) as ObjectResult;
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(result, response.Value);
+    }
+
+    [Fact]
+    public async Task RecoverByEmailAsync_ShouldReturnOk_WhenUseCaseSucceeds()
+    {
+        // Arrange
+        var mockUseCase = new Mock<IRecoverByEmailUseCase>();
+        var email = "john.doe@example.com";
+        var result = new Result<ResponseClient>();
+        result.Succeeded(new ResponseClient(Guid.NewGuid(), DateTime.UtcNow, "John Doe", "john.doe@example.com", "12345678900"));
+
+        mockUseCase.Setup(x => x.RecoverByEmailAsync(email)).ReturnsAsync(result);
+
+        var controller = new ClientController();
+
+        // Act
+        var response = await controller.RecoverByEmailAsync(mockUseCase.Object, email) as ObjectResult;
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(result, response.Value);
+    }
+
+    [Fact]
+    public async Task RecoverByCPFAsync_ShouldReturnOk_WhenUseCaseSucceeds()
+    {
+        // Arrange
+        var mockUseCase = new Mock<IRecoverByCPFUseCase>();
+        var cpf = "12345678900";
+        var result = new Result<ResponseClient>();
+        result.Succeeded(new ResponseClient(Guid.NewGuid(), DateTime.UtcNow, "John Doe", "john.doe@example.com", "12345678900"));
+
+        mockUseCase.Setup(x => x.RecoverByCPFAsync(cpf)).ReturnsAsync(result);
+
+        var controller = new ClientController();
+
+        // Act
+        var response = await controller.RecoverByCPFAsync(mockUseCase.Object, cpf) as ObjectResult;
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(result, response.Value);
+    }
+}
---
+++ Health.Med.Client/Client.Tests/UseCases/LoginUseCaseTests.cs
@@ -0,0 +1,93 @@
+﻿using Client.Application.UseCase.Login;
+using Client.Communication.Request;
+using Client.Domain.Repositories.Contracts;
+using Moq;
+using Serilog;
+using TokenService.Manager.Controller;
+
+namespace Client.Tests.UseCases;
+
+public class LoginUseCaseTests
+{
+    private readonly Mock<IClientReadOnly> _clientReadOnlyMock;
+    private readonly Mock<PasswordEncryptor> _passwordEncryptorMock;
+    private readonly Mock<TokenController> _tokenControllerMock;
+    private readonly Mock<ILogger> _loggerMock;
+    private readonly LoginUseCase _useCase;
+
+    public LoginUseCaseTests()
+    {
+        _clientReadOnlyMock = new Mock<IClientReadOnly>();
+        _passwordEncryptorMock = new Mock<PasswordEncryptor>("any-string");
+        _tokenControllerMock = new Mock<TokenController>(1000, "TWluaGFDaGF2ZVNlY3JldGExMjM=");
+        _loggerMock = new Mock<ILogger>();
+
+        _useCase = new LoginUseCase(
+            _clientReadOnlyMock.Object,
+            _passwordEncryptorMock.Object,
+            _tokenControllerMock.Object,
+            _loggerMock.Object
+        );
+    }
+
+    [Fact]
+    public async Task LoginAsync_ShouldReturnSuccess_WhenLoginIsSuccessful()
+    {
+        // Arrange
+        var request = new RequestLoginClient("any@email.com", "password");
+        var encryptedPassword = GetEncryptedPassword();
+        var doctor = new Domain.Entities.Client(Guid.NewGuid(), DateTime.UtcNow, "Client Name", "Client", "email@example.com", "12345", encryptedPassword);
+
+        _clientReadOnlyMock
+            .Setup(x => x.RecoverByEmailPasswordAsync(request.Email, It.IsAny<string>()))
+            .ReturnsAsync(doctor);
+
+        // Act
+        var result = await _useCase.LoginAsync(request);
+
+        // Assert
+        Assert.True(result.Success);
+        Assert.NotNull(result.Data);
+        Assert.Equal(doctor.Name, result.Data.Name);
+        Assert.Equal(doctor.Email, result.Data.Email);
+    }
+
+    [Fact]
+    public async Task LoginAsync_ShouldReturnFailure_WhenDoctorNotFound()
+    {
+        // Arrange
+        var request = new RequestLoginClient("any@email.com", "password");
+
+        _clientReadOnlyMock
+            .Setup(x => x.RecoverByEmailPasswordAsync(request.Email, It.IsAny<string>()))
+            .ReturnsAsync(new Domain.Entities.Client());
+
+        // Act
+        var result = await _useCase.LoginAsync(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.Contains("E-mail ou senha incorretos", result.Errors);
+    }
+
+    [Fact]
+    public async Task LoginAsync_ShouldReturnFailure_WhenUnexpectedExceptionOccurs()
+    {
+        // Arrange
+        var request = new RequestLoginClient("any@email.com", "password");
+
+        _clientReadOnlyMock
+            .Setup(x => x.RecoverByEmailPasswordAsync(request.Email, It.IsAny<string>()))
+            .Throws(new Exception("Unexpected error"));
+
+        // Act
+        var result = await _useCase.LoginAsync(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.Contains("Algo deu errado: Unexpected error", result.Errors);
+    }
+
+    private static string GetEncryptedPassword() =>
+       "207c5533da953c64419d3e7662e290f9d3b41745d3eb5dbab255a3002053d0a8c0a31f43f4e0db01a55f55b5ea694dd44c211178120b81dcf48ee55bc367c4fe";
+}
