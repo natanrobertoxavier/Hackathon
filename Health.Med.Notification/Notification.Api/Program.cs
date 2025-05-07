cff855b39fc4afa0a748caf3ef7ad00e4d2f1107
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2025-05-07 09:17:23 -0300

    notification: added health endpoint

--- Health.Med.Notification/Notification.Api/Program.cs
+++ Health.Med.Notification/Notification.Api/Program.cs
@@ -1,6 +1,10 @@
 using Notification.Infrastructure;
 using Notification.Application;
 using Notification.Api;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Prometheus;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +17,18 @@ builder.Services.AddConfiguration(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddHealthChecks()
+    .AddCheck("self", () => HealthCheckResult.Healthy());
+
 var app = builder.Build();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.UseMetricServer();
+app.UseHttpMetrics();
 
 if (app.Environment.IsDevelopment())
 {
